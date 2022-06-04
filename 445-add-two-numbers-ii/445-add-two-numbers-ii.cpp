a75c2059db162cb3eb132db5262ab541064241bb
Faisal  Ahmad <faisalahmad@protonmail.com>
2022-06-04 11:17:58 +0300

    Time: 66 ms (24.05%), Space: 71.2 MB (68.79%) - LeetHub

---
+++ 445-add-two-numbers-ii/445-add-two-numbers-ii.cpp
@@ -0,0 +1,81 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(!head){
+            return head;
+        }
+        ListNode* prev = nullptr; ListNode* curr = head; ListNode* next;
+        while(curr){
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+    
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        l1 = reverseList(l1);
+        l2 = reverseList(l2);
+        int carry = 0;
+        auto temp1 = l1;
+        auto temp2 = l2;
+        ListNode* newHead = NULL;
+        ListNode* newTail = newHead;
+        while(temp1 && temp2){
+            int val = (temp1->val + temp2->val + carry);
+            carry = val/10;
+            val %= 10;
+            if(!newHead){
+                newHead = new ListNode(val);
+                newTail = newHead;
+            }else{
+                newTail->next = new ListNode(val);
+                newTail = newTail -> next;
+            }
+            temp1 = temp1->next;
+            temp2 = temp2->next;
+        }
+        while(temp1){
+            int val = (temp1->val + carry);
+            carry = val/10;
+            val %= 10;
+            if(!newHead){
+                newHead = new ListNode(val);
+                newTail = newHead;
+            }else{
+                newTail->next = new ListNode(val);
+                newTail = newTail -> next;
+            }
+            temp1 = temp1->next;
+        }
+        while(temp2){
+            int val = (temp2->val + carry);
+            carry = val/10;
+            val %= 10;
+            if(!newHead){
+                newHead = new ListNode(val);
+                newTail = newHead;
+            }else{
+                newTail->next = new ListNode(val);
+                newTail = newTail -> next;
+            }
+            temp2 = temp2->next;
+        }
+        if(carry == 1){
+            newTail->next = new ListNode(carry);
+        }
+        newHead = reverseList(newHead);
+        return newHead;
+    }
+};
