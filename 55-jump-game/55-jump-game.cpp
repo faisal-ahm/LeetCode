61d1826a0c0a9bb49e431e2ea25bdffccc3eddaa
Faisal  Ahmad <faisalahmad@protonmail.com>
2022-06-07 12:08:33 +0300

    Time: 102 ms (43.28%), Space: 48.2 MB (93.40%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,28 +1,12 @@
 class Solution {
 public:
-    
-    int helper(vector<int>& nums, vector<int>& dp, int start = 0){
-        int n = nums.size();
-        if(start >= n-1){
-            return dp[start] = 1;
-        }
-        if(nums[start] == 0){
-            return dp[start] = 0;
-        }
-        if(dp[start] != -1){
-            return dp[start];
-        }
-        int ans = 0;
-        for(int i = 1; i <= nums[start]; i++){
-            if(helper(nums,dp,start + i)){
-                ans = 1;
-                break;
+    bool canJump(vector<int>& nums) {
+        int lastGoodIndex = nums.size()-1;
+        for(int i = nums.size()-1; i >= 0; i--){
+            if(i + nums[i] >= lastGoodIndex){
+                lastGoodIndex = i;
             }
         }
-        return dp[start] = ans;
-    }
-    bool canJump(vector<int>& nums) {
-        vector<int>dp(nums.size()+1, -1);
-        return helper(nums,dp) == 0 ? false : true;
+        return lastGoodIndex == 0;
     }
 };
