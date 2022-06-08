dd7795fad581ccabb392ab8c8e2fb26a97fa805d
Faisal  Ahmad <faisalahmad@protonmail.com>
2022-06-08 11:38:23 +0300

    Time: 287 ms (8.84%), Space: 85.2 MB (8.53%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    
+    void helper(vector<int>& candidates, int target, int currSum, int currIndex, vector<int>currComb, vector<vector<int>>&ans){
+        if(currSum > target || currIndex >= candidates.size()){
+            return;
+        }
+        if(currSum == target){
+            ans.push_back(currComb);
+            return;
+        }
+        currComb.push_back(candidates[currIndex]);
+        helper(candidates, target, currSum + candidates[currIndex], currIndex, currComb, ans);
+        currComb.pop_back();
+        helper(candidates,target,currSum,currIndex + 1, currComb, ans);
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>>ans;
+        vector<int>currComb;
+        helper(candidates,target, 0,0,currComb,ans);
+        return ans;
+    }
+};
