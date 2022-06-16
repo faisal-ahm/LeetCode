171d4e1f7f0ea37e74faa446b32feea9694ad586
Faisal  Ahmad <faisalahmad@protonmail.com>
2022-06-16 21:30:10 +0300

    Time: 42 ms (23.90%), Space: 18.2 MB (23.14%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -2,22 +2,22 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
-        vector<int>candies(n, 1);
+        vector<int>l2r(n,1);
+        vector<int>r2l(n,1);
         for(int i = 1; i < n; i++){
-            if(ratings[i] > ratings[i - 1]){
-                candies[i] = 1 + candies[i-1];
+            if(ratings[i] > ratings[i-1]){
+                l2r[i] = 1 + l2r[i-1];
             }
-            cout << candies[i] << " ";
         }
-        cout << endl;
-        int ans = 0;
         for(int i = n-2; i >= 0; i--){
-            if(ratings[i] > ratings[i + 1]){
-                candies[i] = max(candies[i], 1 + candies[i+1]);
+            if(ratings[i] > ratings[i+1]){
+                r2l[i] = 1 + r2l[i+1];
             }
-            ans += candies[i];
-            cout << candies[i] << " ";
         }
-        return ans = ans + candies[n-1];
+        int ans = 0;
+        for(int i = 0; i < n; i++){
+            ans += max(r2l[i], l2r[i]);
+        }
+        return ans;
     }
 };
