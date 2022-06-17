672f9787b65ae1628a89a12a2921239d1d70310c
Faisal  Ahmad <faisalahmad@protonmail.com>
2022-06-17 14:15:40 +0300

    Time: 0 ms (100.00%), Space: 7.3 MB (97.10%) - LeetHub

--- 486-predict-the-winner/486-predict-the-winner.cpp
+++ 486-predict-the-winner/486-predict-the-winner.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
     bool PredictTheWinner(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>>dp(n, vector<int>(n));
+        vector<int>dp(n);
         for(int s = n-1; s >= 0; s--){
             for(int e = s; e < n; e++){
                 if(e == s){
-                    dp[s][e] = nums[s];
+                    dp[e] = nums[s];
                     continue;
                 }
-                int a = nums[s] - dp[s+1][e];
-                int b = nums[e] - dp[s][e-1];
-                dp[s][e] = max(a,b);
+                int a = nums[s] - dp[e];
+                int b = nums[e] - dp[e-1];
+                dp[e] = max(a,b);
             }
         }
-        return dp[0][n-1] >= 0;
+        return dp[n-1] >= 0;
     }
 };
