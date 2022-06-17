bc6a244896747b7fda10374b80793616d627151e
Faisal  Ahmad <faisalahmad@protonmail.com>
2022-06-17 18:36:04 +0300

    Added solution - LeetHub

---
+++ Rotten Oranges - GFG/rotten-oranges.cpp
@@ -0,0 +1,82 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution 
+{
+    public:
+    //Function to find minimum time required to rot all oranges. 
+    int orangesRotting(vector<vector<int>>& grid) {
+        int numRows = grid.size(), numCols = grid[0].size(), numFresh = 0;
+        queue<pair<int,int>>q;
+        for(int i = 0; i < numRows; i++){
+            for(int j = 0; j < numCols; j++){
+                if(grid[i][j] == 2){
+                    q.push({i,j});
+                }
+                if(grid[i][j] == 1){
+                    numFresh++;
+                }
+            }
+        }
+        if(numFresh==0){
+            return 0;
+        }
+        q.push({-1,-1});
+        int ans = -1;
+        while(!q.empty()){
+            auto front = q.front();
+            q.pop();
+            if(front.first == -1){
+                ans++;
+                if(!q.empty()){
+                    q.push({-1,-1});
+                }
+            }else{
+                int x = front.first, y = front.second;
+                if(x > 0 && grid[x-1][y] ==1){
+                    grid[x-1][y] = 2;
+                    q.push({x-1, y});
+                    numFresh--;
+                }
+                if(x < numRows-1 && grid[x+1][y] == 1){
+                    grid[x+1][y] = 2;
+                    q.push({x+1, y});
+                    numFresh--;
+                }
+                if(y > 0 && grid[x][y-1] ==1){
+                    grid[x][y-1] = 2;
+                    q.push({x, y-1});
+                    numFresh--;
+                }
+                if(y < numCols-1 && grid[x][y+1] ==1){
+                    grid[x][y+1] = 2;
+                    q.push({x, y+1});
+                    numFresh--;
+                }
+            }
+        }
+        return numFresh == 0 ? ans : -1;
+    }
+};
+
+// { Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n, m;
+		cin >> n >> m;
+		vector<vector<int>>grid(n, vector<int>(m, -1));
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < m; j++){
+				cin >> grid[i][j];
+			}
+		}
+		Solution obj;
+		int ans = obj.orangesRotting(grid);
+		cout << ans << "\n";
+	}
+	return 0;
+}  // } Driver Code Ends
