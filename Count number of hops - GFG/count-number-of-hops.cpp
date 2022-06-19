61e646087856235f9274d1338075a68768c60b40
Faisal  Ahmad <faisalahmad@protonmail.com>
2022-06-19 14:21:26 +0300

    Added solution - LeetHub

--- Count number of hops - GFG/count-number-of-hops.cpp
+++ Count number of hops - GFG/count-number-of-hops.cpp
@@ -18,13 +18,17 @@ class Solution
             return 4;
         }
         vector<long long>dp(n+1);
-        dp[1] = 1;
-        dp[2] = 2;
-        dp[3] = 4;
+        long long a = 1;
+        long long b = 2;
+        long long c = 4;
+        long long d;
         for(int i = 4; i <= n; i++){
-            dp[i] = (dp[i-1]%M + dp[i - 2]%M + dp[i-3]%M)%M;
+            d = (a%M + b%M + c%M)%M;
+            a= b;
+            b = c;
+            c = d;
         }
-        return dp[n];
+        return d;
     }
 };
 
