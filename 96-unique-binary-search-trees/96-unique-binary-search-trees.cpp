370ddee7456f8a5b50f6eeafe820adaa08341719
Faisal  Ahmad <faisalahmad@protonmail.com>
2022-06-28 20:57:40 +0300

    Time: 3 ms (38.47%), Space: 6 MB (65.10%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
@@ -12,7 +12,20 @@ public:
         return dp[n] = ans;
     }
     int numTrees(int n){
-        vector<int>dp(n + 1, -1);
-        return numTrees(n, dp);
+        // vector<int>dp(n + 1, -1);
+        // return numTrees(n, dp);
+        if(n <= 1) return 1;
+        vector<int>dp(n + 1);
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i = 2; i <= n; i++){
+            dp[i] = 0;
+            for(int j = 1; j <= i; j++){
+                int numLeft = j - 1;
+                int numRight = i - j;
+                dp[i] += dp[numLeft] * dp[numRight];
+            }
+        }
+        return dp[n];
     }
 };
