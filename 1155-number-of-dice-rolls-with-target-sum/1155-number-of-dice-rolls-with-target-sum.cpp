45aa8a0b8829044a5e57ff88c9daf0ed4a3bef5b
Faisal  Ahmad <faisalahmad@protonmail.com>
2022-07-05 08:02:10 +0300

    Time: 52 ms (47.03%), Space: 6.6 MB (65.98%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -1,35 +1,26 @@
 
 class Solution {
 public:
-    int numRollsToTarget(int n, int k, int target) {
-        int mod = 1000000007;
-        vector<vector<int>> dp(n + 1, vector<int>(target + 1));
-        for(int i = 0; i <= n; i++){
-            for(int j = 0; j <= target; j++){
-                if(i == 0){
-                    dp[i][j] = 0;
-                    if(j == 0) dp[i][j] = 1;
-                    continue;
-                }
-                dp[i][j] = 0;
-                for(int m = 1; m <= k && m <= j; m++){
-                    dp[i][j] += dp[i - 1][j - m]%(mod);
-                    dp[i][j] %= mod;
-                }
+    
+int numRollsToTarget(int n, int k, int target) {
+    int mod = 1000000007;
+    vector<int>dp(target + 1);
+    for(int i = 0; i <= n; i++){
+        for(int j = target; j >= 0; j--){
+            if(i == 0){
+                dp[j] = 0;
+                if(j == 0) dp[j] = 1;
+                continue;
             }
+            int ans = 0;
+            for(int m = 1; m <= k && m <= j; m++){
+                ans += dp[j - m]%(mod);
+                ans %= mod;
+            }
+            dp[j] = ans;
         }
-        
-        return dp[n][target];
-        
-        
-        if(n == 0){
-            if(target == 0)return 1;
-            return 0;
-        }
-        int ans = 0;
-        for(int i = 1; i <= k; i++){
-            ans += numRollsToTarget(n-1, k, target - i);
-        }
-        return ans;
     }
+        
+    return dp[target];
+}
 };
