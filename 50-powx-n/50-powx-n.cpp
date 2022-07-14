c9e6acd175949658636740d2bd504cb6992a0ad3
Faisal  Ahmad <faisalahmad@protonmail.com>
2022-07-14 19:15:14 +0300

    Time: 6 ms (14.09%), Space: 5.8 MB (79.99%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,9 +1,17 @@
 class Solution {
 public:
-    double myPow(double x, long n) {
-        if(n == 0 || x == 1) return 1.0;
-        if(x == -1) return n%2 ? -1 : 1;
-        if(n < 0) return 1/myPow(x, -1 * n);
-        return (n%2) ? x * myPow(x*x, n/2) : myPow(x*x, n/2);
+ double myPow(double x, long n) { 
+    if(n==0) return 1;
+    if(n<0) {
+        n = -n;
+        x = 1/x;
     }
+    double ans = 1;
+    while(n>0){
+        if(n&1) ans *= x;
+        x *= x;
+        n >>= 1;
+    }
+    return ans;
+}
 };
