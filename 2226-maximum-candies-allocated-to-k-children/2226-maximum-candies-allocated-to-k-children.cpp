d0fa7a39db6f293950217418dc320f1ab6d14642
Faisal  Ahmad <faisalahmad@protonmail.com>
2022-07-19 22:26:13 +0530

    Time: 1127 ms (5.13%), Space: 84.4 MB (14.61%) - LeetHub

---
+++ 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int maximumCandies(vector<int>& candies, long long k) {
+    long long sum = 0, n = candies.size();
+    for(int i = 0; i < n; i++){
+        sum += (long)candies[i];
+        cout << "hi1" << endl;
+    }
+
+    long long left = 0;
+    long long right = sum/k;
+    while(left < right){
+
+        cout << left << " " << right << endl;
+        long long mid = (left + right + 1)/2;
+        long long numKidsPossible = 0;
+
+        for(int i = 0; i < n && numKidsPossible <= k; i++){
+            numKidsPossible += ((long long)candies[i])/mid;
+
+        }
+        if(numKidsPossible >= k){
+            left = mid;
+        }else{
+            right = mid-1;
+        }
+    }
+    return left;
+}
+};
