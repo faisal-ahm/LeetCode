11a9f5bfc8d323b8656108a840b7b54b03f1731b
Faisal  Ahmad <faisalahmad@protonmail.com>
2022-07-20 19:48:42 +0530

    Time: 3 ms (68.61%), Space: 7 MB (99.95%) - LeetHub

---
+++ 410-split-array-largest-sum/410-split-array-largest-sum.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int splitArray(vector<int>& nums, int m) {
+        int l = 0, r = 1e9, n = nums.size();
+        for(int x : nums){
+            l = max(l, x);
+        }
+        
+        if(l==0)return 0;
+        
+        while(l < r){
+            int mid = l + (r - l)/2, curr = 0, numPartitions = 1;
+            for(int i = 0; i < n && numPartitions <= m; i++){
+                if(curr + nums[i] > mid){
+                    curr = 0;
+                    numPartitions++;
+                }
+                curr+=nums[i];
+            }
+            if(numPartitions > m){
+                l = mid + 1;
+            }else{
+                r = mid;
+            }
+        }
+        return l;
+    }
+};
