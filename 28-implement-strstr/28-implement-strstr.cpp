9bf6a1d096a4356f7e012be5fc99a09ee3d51768
Faisal  Ahmad <faisalahmad@protonmail.com>
2022-07-28 13:41:04 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (90.45%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -1,21 +1,48 @@
 class Solution {
 public:
-    int strStr(string haystack, string needle) {
-        int n = haystack.size(), m = needle.size(), i = 0;
+    vector<int> getLPS(string& s){
+        int n = s.size();
+        vector<int>lps(n, 0);
+        int i = 1, j = 0;
         while(i < n){
-            if(haystack[i] != needle[0]){
+            if(s[i] == s[j]){
+                lps[i] = j + 1;
+                j++;
                 i++;
-                continue;
+            }else{
+                if(j!=0){
+                    j = lps[j-1];
+                }else{
+                    i++;
+                }
             }
-            int j = 0;
-            cout << i << " -> " ;
-            while((i+j)<n && j<m && haystack[i+j] == needle[j]){
-                cout << j << " ";
+        }
+        return lps;
+    }
+    int strStr(string haystack, string needle) {
+        int n = haystack.size(), m = needle.size();
+        
+        if(n==0) return -1;
+        if(m==0) return 0;
+        
+        int i = 0, j = 0;
+        
+        vector<int> lps = getLPS(needle);
+        while(i < n){
+            if(haystack[i] == needle[j]){
+                i++;
                 j++;
             }
-            cout << j << endl;
-            if(j==m)return i;
-            i++;
+            
+            if(j == m)return i-j;
+            
+            if(i < n && haystack[i] != needle[j]){
+                if(j > 0){
+                    j = lps[j-1];
+                }else{
+                    i++;
+                }
+            }
         }
         return -1;
     }
