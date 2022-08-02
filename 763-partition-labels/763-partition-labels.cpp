d32e715b63c0eb1d3d649f2995484ce61718f714
Faisal  Ahmad <faisalahmad@protonmail.com>
2022-08-02 23:35:19 +0530

    Time: 12 ms (18.82%), Space: 6.6 MB (68.52%) - LeetHub

--- 763-partition-labels/763-partition-labels.cpp
+++ 763-partition-labels/763-partition-labels.cpp
@@ -4,10 +4,13 @@ public:
         vector<int> ans, pos(26,-1);
         int n = s.size();
         for(int i = 0; i < n; i++) pos[s[i]-'a'] = i;
-        int end = INT_MIN, size = 0;
+        
+        int end = -1, size = 0;
+        
         for(int i = 0; i < n; i++){
             end = max(end, pos[s[i] - 'a']);
             size++;
+            
             if(i == end){
                 ans.push_back(size);
                 size = 0;
