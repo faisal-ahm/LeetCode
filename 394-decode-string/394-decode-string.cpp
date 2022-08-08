f6df998cf696aed7be3adc5d043c05dd196d513e
Faisal  Ahmad <faisalahmad@protonmail.com>
2022-08-08 23:16:34 +0530

    Time: 3 ms (51.02%), Space: 6.5 MB (56.76%) - LeetHub

---
+++ 394-decode-string/394-decode-string.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string help(string& s, int& i){
+        int n = 0;
+        string word = "";
+        for(; i < s.size(); i++){
+            char c = s[i];
+            if(c == ']'){
+                return word;
+            }else if(c == '['){
+                i++;
+                string res = help(s,i);
+                for(; n > 0; n--)word+=res;
+            }else if(isdigit(c)){
+                n = n*10 + c-'0';
+            }else{
+                word+=c;
+            }
+        }
+        return word;
+    }
+    string decodeString(string s) {
+        int i = 0;
+        return help(s,i);
+    }
+};
