4e6d0d7e7fb2515d294c1d5253ed8ebbbcbe5468
Faisal  Ahmad <faisalahmad@protonmail.com>
2022-08-09 16:06:30 +0530

    Time: 4 ms (64.47%), Space: 6.5 MB (87.35%) - LeetHub

--- 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     string removeDuplicateLetters(string s) {
-        vector<int>lastIndex(26);
         int n = s.size();
+        vector<bool>vis(26, false);
+        vector<int>idx(26, -1);//last index of each letter in s
         for(int i = 0; i < n; i++){
-            lastIndex[s[i] - 'a'] = i;
+            idx[s[i]-'a'] = i;
         }
-        vector<bool>vis(26,false);
         string ans = "";
         for(int i = 0; i < n; i++){
-            char c = s[i];
-            if(vis[c-'a']){
-                continue;
-            }
-            while(ans.size() && ans.back() > c && lastIndex[ans.back() - 'a'] > i){
+            if(vis[s[i]-'a']) continue;
+            while(!ans.empty() && ans.back() > s[i] && idx[ans.back()-'a'] > i){
                 vis[ans.back() - 'a'] = false;
                 ans.pop_back();
             }
-            ans.push_back(c);
-            vis[c-'a'] = true;
+            ans+=s[i];
+            vis[s[i]-'a'] = true;
         }
         return ans;
     }
