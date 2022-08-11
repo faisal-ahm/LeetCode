f1cf69b150db456b801baf83549aa1242ccabec0
Faisal  Ahmad <faisalahmad@protonmail.com>
2022-08-11 15:09:17 +0530

    Time: 0 ms (100.00%), Space: 9.9 MB (94.98%) - LeetHub

--- 100-same-tree/100-same-tree.cpp
+++ 100-same-tree/100-same-tree.cpp
@@ -12,16 +12,8 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if(!p && !q){
-            return true;
-        }else if(!p){
-            return false;
-        }else if(!q){
-            return false;
-        }
-        if(p->val != q->val){
-            return false;
-        }
-        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+        if(!p || !q) return p == q;
+        
+        return p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
     }
 };
