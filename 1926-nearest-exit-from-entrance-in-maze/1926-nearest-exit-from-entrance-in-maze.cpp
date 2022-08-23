00a4072d45d5cdd7633873e3e54f351be564f962
Faisal  Ahmad <faisalahmad@protonmail.com>
2022-08-24 00:07:01 +0530

    Time: 445 ms (5.76%), Space: 35 MB (32.78%) - LeetHub

---
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    void print(vector<vector<int>>& grid){
+        for(auto& i : grid){
+            for(int x : i){
+                cout << x << " ";
+            }
+            cout << endl;
+        }
+    }
+    int nearestExit(vector<vector<char>>& maze, vector<int>& entr) {
+        int m = maze.size(), n = maze[0].size();
+        vector<vector<int>>grid(m, vector<int>(n));
+        
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(maze[i][j] == '+') {
+                    grid[i][j] = -2;
+                    continue;
+                }
+                
+                grid[i][j] = -1;
+            }
+        }
+        
+        queue<pair<int,int>>q;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if((i == entr[0] && j == entr[1]) || maze[i][j] == '+') continue;
+                if(i == 0 || j == 0 || i == m-1 || j == n-1){
+                    grid[i][j] = 0;
+                    q.push({i,j});
+                }
+            }
+        }
+        print(grid);
+        while(q.size()){
+            int i = q.front().first, j = q.front().second, val = grid[i][j];
+            q.pop();
+            if(i > 0 && grid[i-1][j] == -1){
+                grid[i-1][j] = val + 1;
+                q.push({i-1, j});
+            }
+            if(j > 0 && grid[i][j-1] == -1){
+                grid[i][j-1] = val + 1;
+                q.push({i, j-1});
+            }
+            if(i < m-1 && grid[i+1][j] == -1){
+                grid[i+1][j] = val + 1;
+                q.push({i+1, j});
+            }
+            if(j < n-1 && grid[i][j+1] == -1){
+                grid[i][j+1] = val + 1;
+                q.push({i, j+1});
+            }
+        }
+        
+        return grid[entr[0]][entr[1]];
+    }
+};
