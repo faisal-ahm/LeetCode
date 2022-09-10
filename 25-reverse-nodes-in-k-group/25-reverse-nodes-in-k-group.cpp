7a1b771957bdc0c60c626d82871fd1298c534a6d
Faisal  Ahmad <faisalahmad@protonmail.com>
2022-09-10 11:46:11 +0530

    Time: 11 ms (98.56%), Space: 11.5 MB (72.36%) - LeetHub

--- 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.cpp
+++ 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.cpp
@@ -18,28 +18,42 @@ public:
         }
         return ans;
     }
-    ListNode* help(ListNode* head, int k, int n){
-        if(n < k) return head;
-        ListNode* tail = head;
-        int K = k-1;
-        while(K--){
-            tail = tail->next;
-        }
-        auto nextHead = tail->next;
-        tail->next = NULL;
-        auto curr = head;
-        ListNode* prev = NULL; ListNode* next;
-        while(curr){
-            next = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = next;
-        }
-        head->next = help(nextHead, k, n-k);
-        return prev;
-    }
+    
     ListNode* reverseKGroup(ListNode* head, int k) {
         int n = getn(head);
-        return help(head, k, n);
+        ListNode* newHead = NULL; 
+        ListNode* prevSegTail = NULL;
+        ListNode* tempHead = head;
+        
+        while(n >= k){
+            int K = k-1;
+            auto currTail = tempHead;
+            while(K--) currTail = currTail->next;
+            auto nextSeg = currTail->next;
+            currTail->next = NULL;
+            
+            ListNode* curr = tempHead;
+            ListNode* prev = NULL;
+            ListNode* next;
+            
+            while(curr){
+                next = curr->next;
+                curr->next = prev;
+                prev = curr;
+                curr = next;
+            }
+            
+            if(!newHead){
+                newHead = prev;
+            }else{
+                prevSegTail->next = prev;
+            }
+            
+            tempHead->next = nextSeg;
+            prevSegTail = tempHead;
+            tempHead = nextSeg;
+            n-=k;
+        }
+        return newHead;
     }
 };
