3775fc6ab5bd4e2ee81d3db24b580571a49516b2
Faisal  Ahmad <faisalahmad@protonmail.com>
2022-09-12 10:07:39 +0530

    Time: 4 ms (57.54%), Space: 7.3 MB (57.35%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,24 +1,16 @@
 class Solution {
 public:
-    void helper(vector<int>& nums, vector<vector<int>>&ans, int start = 0){
-        int n = nums.size();
-        if(start == n){
-            ans = {{}};
-            return;
-        }
-        helper(nums,ans,start + 1);
-        int prevSize = ans.size();
-        for(int i = 0; i < prevSize; i++){
-            vector<int>row = {nums[start]};
-            for(int x : ans[i]){
-                row.push_back(x);
-            }
-            ans.push_back(row);
-        }
-    }
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> ans;
-        helper(nums, ans);
-        return ans;
-    }
+vector<vector<int>> subsets(vector<int>& nums){
+	vector<vector<int>> ans;
+	int n = nums.size(), p = 1 << n;
+
+	for(int i = 0; i < p; i++){
+		vector<int>currSet;
+		for(int j = 0; j < n; j++){
+			if((i >> j) & 1) currSet.push_back(nums[j]);
+		}
+		ans.push_back(currSet);
+	}
+	return ans;
+}
 };
